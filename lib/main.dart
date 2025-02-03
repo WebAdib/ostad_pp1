5c5e30595ab2ff76573f4bb9a2e1ec14f49091a8
webadib <webadib@gmail.com>
2025-02-03 21:12:14 +0600

    class 3 part 1

--- lib/main.dart
+++ lib/main.dart
@@ -284,6 +284,49 @@ class UserInfo extends StatelessWidget {
                     }),
               ),
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => UserDetails(name: 'Adib')));
+              },
+              child: Text('Next Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// user details page
+class UserDetails extends StatelessWidget {
+  final String name;
+  const UserDetails({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'User Details',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("User Details - ${name}"),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Back"),
+            ),
           ],
         ),
       ),
